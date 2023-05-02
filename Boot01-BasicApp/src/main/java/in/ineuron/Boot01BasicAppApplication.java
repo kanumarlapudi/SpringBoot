640250b6f798586eea82e562874fce807146611c
kanumarlapudi <180020034ce@gmail.com>
2023-05-02 14:43:52 +0530

    first

---
+++ Boot01-BasicApp/src/main/java/in/ineuron/Boot01BasicAppApplication.java
@@ -0,0 +1,16 @@
+package in.ineuron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Boot01BasicAppApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(Boot01BasicAppApplication.class, args);
+		System.out.println(context.getClass().getName());
+	}
+
+}
