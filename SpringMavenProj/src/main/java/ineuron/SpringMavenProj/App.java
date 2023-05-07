71c021d8416d88134166bb2d972f24574574ffcc
kanumarlapudi <180020034ce@gmail.com>
2023-05-07 19:41:26 +0530

    first

---
+++ SpringMavenProj/src/main/java/ineuron/SpringMavenProj/App.java
@@ -0,0 +1,13 @@
+package ineuron.SpringMavenProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
